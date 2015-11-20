8bd67e871521196c14198dcc5026fb59f87337e8
JavaProcessingOctopus <javaprocessingoctopus@outlook.com>
2015-11-20 22:07:21 +0100

    Module revus
    Correction de la logique d'import/export des modules et mise a jouer des
    fonctionnalité des modules.
    - reste test a effectuer
    - reste changeDir a compléter
    - reste checkName a completer

--- Jeu.js
+++ Jeu.js
@@ -1,67 +1,177 @@
-Jouuer = require('./joueur.js')
-module.exports = 
-	
-	function() {
-		this.joueurs = [];
-		this.remplissage = [];
-	    this.active = false;
-	    //this.HAUTEUR
-	    //this.LARGEUR
-		
-		this.start = function(){	//
-			this.active = true;
-		};
-		
-		this.addJoueur = function(joueur){	 //ajoute le nouveau joeur a this.joueurs et donne une coordonnee libre a joueur
-			this.joueurs.push(joueur);
-			joueur.position = coorLibre();
-		};
-		
-		this.coorLibre = function(){	//renvois une coordonnée libre
-			//fetching default starting points
-			
-			//chercher un espace libre a partir d'une coordonnée prédefinis ( || aléatoire?)
-		};
-		
-		this.changeDirections = function(nameAndDirection){};
+var Coordonnee = require("Coordonnee.js");
+var Joueur = require('./Joueur.js');
 
-		this.next = function(){	 //fais avancer tous les joueur et met a jour les trace
-			for(i = 0; i < joueurs.length(); i++) {
-				jo = joueurs[i]
-				remplissage.push(new Trace(jo.nom, jo.position.copy()))
-				jo.avancer();
-			}
-		};
+var joueurs = [];
+var canvas = [];
+var active = false;
+var HAUTEUR = 10;
+var LARGEUR = 10;
 		
-		this.state = function(){}; //renvois le fichier JSON a donner au client
+var start = function () {	//
+	active = true;
+};
 		
-		this.collision = function(){	//renvois les joueurs qui on fait une collision
-			res = [];
-			for(i=0; i<remplissage.length(); i++){
-				for(j=0; i<joueurs.length(); i++){
-					if(remplissage[i].position.equals(joueurs[j].position)){
-						res.push(joueurs[j]);
-						//retirer j de joueurs?
-					}
-				}
-			}
-			return res;
-		};
+var coorLibre = function () {
+    if (typeof(canvas[2][2]) !== 'undefined') { //exemple de test pour une coordonnée prefférer de dépard
+        return new Coordonnee(2, 2);
+    }
+    
+    //parcous de tout le canvas
+    for (i = 0;  i < LARGEUR-1; i++) {
+        for (j = 0; j < HAUTEUR; j++) {
+            if (typeof(canvas[i][j]) !== 'undefined') { //exemple de test pour une coordonnée prefférer de dépard
+                return new Coordonnee(i, j);
+            }
+        }
+    }
+};
+    
+var defaultDirection = function (coordonnee) { //determine la bonne direction a donner initialement au joueur
+    var direction = null;
+            
+    if (coordonnee.x < HAUTEUR / 2) { //en haut
+        if (coordonnee.y < LARGEUR / 2) { //en haut a gauche
+            direction = "right";
+        } else { //en haut a droite
+            direction = "down";
+        }
+    } else { //en bas
+        if (coordonnee.y < LARGEUR / 2) { //en bas a gauche
+            direction = "up";
+        } else { //en bas a droite
+            direction = "left";
+        }
+    }
+            
+    return direction;
+};
+
+var prendreCoor = function (coordonnee) { //ajoute une marque a canvas a la coordonne
+    canvas[coordonnee.x][coordonnee.y] = true;
+};
+
+var checkName = function (joueur) { //vérifie si un nom est libre et renvois un nom libre si il est pris
+    var joueur = joueur;
+    for (i = 0; i < joueurs.length; i++) {
+        if (joueurs[i] === joueur) {
+            var lastCar = joueur.charAt(joueur.length - 1);
+            if (!isNaN(lastCar)) {
+                //put lastCar++ at joueur.charAt(joueur.length - 1)
+            } else {
+                //put 1 at the end of joueur
+            }
+        }
+    }
+    return joueur;
+};
+
+var addJoueur = function (joueur) {	 //ajoute le nouveau joeur a this.joueurs, donne une coordonnee libre a joueur, et renvois le nom de joueur car il peut etre changer si le nom donner est déja pris
+    var joueur = checkName(joueur);
+    var jo = new Joueur(joueur);
+    jo.position = coorLibre();
+    prendreCoor(jo.position);
+    jo.direction = defaultDirection(jo.position);
+    joueurs.push(jo);
+    return joueur;
+};
 		
-		this.end = function(){	//renvois true si il y a 1 joueur ou moins
-			return ( joueurs.length < 2 );
-		};
+var changeDirections = function (nameAndDirection) {
+    var nom, direction;  // a initialiser en fonction de data recu
+            
+    for (i = 0; i < joueurs.length; i++) {
+        if (joueurs[i].nom === nom) {
+            joueurs[i].nom.changeDirection(direction);
+            break;
+        }
+    }
+};
+
+var next = function () {	 //fais avancer tous les joueur et met a jour les trace
+    for (i = 0; i < joueurs.length(); i++) {
+        joueurs[i].avancer();
+        canvas[joueurs[i].position.x][joueurs[i].position.y] =  true;
+    }
+};
 		
-		this.winner = function(){	//renvois le gagnant
-			if(joueurs.length === 1){
-				return joueurs[0].nom;
-			}
-			return null;
-		};
+var curState = function () { //renvois le fichier JSON a donner au client
+    var table = [];
+    for (i = 0; i < joueurs.length; i++) {
+        var object = {};
+        object.nom = joueurs[i].nom;
+        object.trace = joueurs[i].trace;
+        table.push(object);
+    }
+            
+    return JSON.stringify(table);
+};
+
+var newState = function () { //renvois le fichier JSON a donner au client
+    var table = [];
+    for (i = 0; i < joueurs.length; i++) {
+        var object = {};
+        object.nom = joueurs[i].nom;
+        object.trace = joueurs[i].position;
+        table.push(object);
+    }
+            
+    return JSON.stringify(table);
+};
+
+var outOfCanvas = function (coordonnee) {
+    return coordonnee.x > LARGEUR || coordonnee.x < 0 || coordonnee.y > HAUTEUR || coordonnee.y < 0;
+};
+    
+var collision = function () {	//renvois les joueurs qui on fait une collision
+    var res = [];
+    var found = false;
+    for (i = 0; i < joueurs.length; i++) { //On vérifie pour chaque joueur qu'il n'y a pas de collision
+        if (outOfCanvas(joueurs[i].position)) {
+            res.push(joueurs[i].name);
+            continue;
+        }
+        for (j = 0; j < joueurs.length; j++) { //En parcourant tous les joueurs
+            for (k = 0; k < joueurs[j].trace.length; k++) { //et en inspéctant toutes les coordonnée ou il on été.
+                if (joueurs[i].position.equals(joueurs[j].trace[k])) {
+                    res.push(joueurs[i].name);
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                found = false;
+                break;
+            }
+        }
+    }
+	return res;
+};
+		
+var end = function () {	//renvois true si il y a 1 joueur ou moins
+    return (joueurs.length < 2);
+};
+		
+var winner = function () {	//renvois le gagnant
+    if (joueurs.length === 1) {
+        return joueurs[0].nom;
+    }
+    return null;
+};
 	    
-	    this.reset = function(){
-	    	this.active = false;
-	    	//vide variable
-	    };
-	}
-	
+var reset = function () {
+    active = false;
+    joueurs = [];
+    canvas = [];
+    //vide variable
+};
+
+module.exports.start = start;
+module.exports.active = active;
+module.exports.addJoueur = addJoueur;
+module.exports.changeDirection = changeDirections;
+module.exports.next = next;
+module.exports.curState = curState;
+module.exports.newState = newState;
+module.exports.collision = collision;
+module.exports.end = end;
+module.exports.winner = winner;
+module.exports.reset = reset;
--- Joueur.js
+++ Joueur.js
@@ -1,34 +1,38 @@
-module.exports = {
-    function (nom, direction, coordonee) {
-    Coordonnee = require("Coordonnee.js");
-    
+function Joueur(nom) {
     this.nom = nom;
-    this.direction = direction || null;
-    this.position = coordonnee || {};
+    this.direction = null;
+    this.position = {};
     this.trace = [];
     
-    this.changeDirection = function (direction){
-        if(direction == "right" || direction == "left"){
-			if(this.direction == "up" || this.direction == "down"){
-				this.direction = direction;
-			}	
-		}else if(direction == "up" || direction =="down"){
-			 if(this.direction == "right" || this.direction == "left"){
-				this.direction = direction;
+    this.changeDirection = function (direction) {
+        if (direction === "right" || direction === "left") {
+            if (this.direction === "up" || this.direction === "down") {
+                this.direction = direction;
+            }
+        } else if (direction === "up" || direction === "down") {
+            if (this.direction === "right" || this.direction === "left") {
+                this.direction = direction;
 			}
-		 }
-        return this
-    };
-    this.avancer = function () {
-        if(this.direction == "right"){
-            this.position.y++; 
-        }else if(this.direction == "left"){
-            this.position.y--;    
-        }else if(this.direction == "up"){ 
-            this.position.x++;
-        }else if(this.direction == "down"){ 
-            this.position.x--;
         }
         return this;
     };
-}}
+    
+    this.avancer = function () {
+        if(this.direction === "right") {
+            this.position.y++;
+            this.trace.push(this.position);
+        } else if (this.direction === "left") {
+            this.position.y--;
+            this.trace.push(this.position);
+        } else if (this.direction === "up") {
+            this.position.x++;
+            this.trace.push(this.position);
+        } else if (this.direction === "down") {
+            this.position.x--;
+            this.trace.push(this.position);
+        }
+        return this;
+    };
+}
+
+module.exports = Joueur;
