e8db19be86287986dc29b792343fbdb1066bcf95
soninaleksandr <sonin.aleksandr@gmail.com>
2015-11-27 15:11:53 +0100

    essai de version latest

---
+++ app1.js
@@ -0,0 +1,65 @@
+app = require('express.io')()
+app.http().io()
+ModuleJeu = require('./Jeu.js');
+
+plBff = []; // buffer de joueurs ? rajouter au jeu
+dirBff = []; //buffer de joeur + direction
+
+//donner la page html au joueur
+// Send the client html.
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/client.html')
+})
+
+app.get('/css/style.css', function(req, res) {
+    res.sendfile(__dirname + '/css/style.css')
+})
+
+app.get('/js/jquery.js', function(req, res) {
+    res.sendfile(__dirname + '/js/jquery.js')
+})
+
+app.get('/js/myscript.js', function(req, res) {
+    res.sendfile(__dirname + '/js/myscript.js')
+})
+
+
+//accepterJoueurs
+app.io.route('ready', function(req){//ajouter un joeur au buffer et sauvegarder son socket
+	plBff.push(req.data);
+	req.io.emit('connected')//envoie les donnee au les joueur
+    //sauvegarder req.io, c'est le socket pour contacter CE joueur.
+})
+
+app.io.route('change-direction', function(req){
+    //ajoute le nom du joeur avec sa direction dans dirBff
+})
+
+function live(){ //boucle du jeu, donne les commande au jeux et contacte les joeurs
+    while(plBff.length>0){ //ajoute les joueur
+        Jeu.addJoueur(plBff.pop());
+    }
+
+    if(Jeu.joueurs.length>1 && !Jeu.active){ //si on a plus d'un joueur et le jeu n'est pas actif
+        Jeu.start(); //on d?mmarre le jeu
+    }
+    if(Jeu.active){
+        while(plBff.length>0){ //change les direction des joeurs
+            Jeu.changeDirection(dirBff.pop());
+        }
+
+        Jeu.next();
+        var morts = Jeu.collision(); //morts recupere un tableau de joeur mort a contacter
+        //...
+        app.broadcast('update', Jeu.state());
+        if(Jeu.end()){
+            var win = jeu.winner(); //on sauvegarde le gagnant qu'on contactera si il y en a un
+            //...
+            Jeu.reset();//on pr?pare le jeux pour la prochaine partis
+        }
+    }
+    setTimeout(live, 500);
+}
+
+
+app.listen(7076)
